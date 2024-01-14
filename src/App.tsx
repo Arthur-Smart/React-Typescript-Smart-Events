87fb618ef0e78bb8168ed75b6e9d965246600cdd
unknown <mwangiarthur777@gmail.com>
2024-01-14 17:06:17 +0300

    design the login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import "./App.css";
+import SignIn from "./pages/signin/SignIn";
 // import Footer from "./components/footer/Footer";
 // import Navbar from "./components/navbar/Navbar";
 // import EventPage from "./pages/eventpage/EventPage";
-import SignUp from "./pages/signup/SignUp";
+// import SignUp from "./pages/signup/SignUp";
 // import Home from "./pages/home/Home";
 
 function App() {
   return (
     <>
-      <SignUp />
+      <SignIn />
     </>
   );
 }
--- src/pages/signin/SignIn.tsx
+++ src/pages/signin/SignIn.tsx
@@ -1,7 +1,49 @@
 import "./signin.css";
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <section className="signin w-full flex flex-col items-center justify-center">
+      <h1 className="font-bold text-[#333333] text-xl">Sign in</h1>
+      <div className="warning bg-[#DBDBDB] rounded-md px-4 py-2 mt-2">
+        <p className="text-[14px]">
+          <i className="fa-solid fa-triangle-exclamation text-orange-400"></i>{" "}
+          Please fill all the input fields
+        </p>
+      </div>
+      <form className="signin-form rounded-md px-3 py-5 mt-7 flex flex-col items-center justify-center border-gray-200 border-[1.5px]">
+        <label className="self-start font-bold text-[#333333] text-base ">
+          Email
+        </label>
+        <div className="flex w-full items-center border-gray-200 border-[1px] py-2 px-3 mt-[2px] rounded-md">
+          <i className="fa-solid fa-envelope text-xl text-[#9A9A9A]"></i>
+          <input
+            type="text"
+            placeholder="Enter email"
+            className="w-11/12 ml-3 outline-0"
+          />
+        </div>
+
+        <label className="self-start font-bold text-[#333333] text-base mt-4">
+          Password
+        </label>
+        <div className="flex w-full items-center border-gray-200 border-[1px] py-2 px-3 mt-[2px] rounded-md">
+          <i className="fa-solid fa-lock text-xl text-[#9A9A9A]"></i>
+          <input
+            type="password"
+            placeholder="Enter password"
+            className="w-11/12 ml-3 outline-0"
+          />
+        </div>
+        <button className="rounded-md w-full py-2 px-3 bg-[#849BF6] text-white mt-6">
+          Sign in
+        </button>
+        <p className="mt-3 text-[#9A9A9A]">
+          Don't have an account?{" "}
+          <span className="underline cursor-pointer">Sign up</span>
+        </p>
+      </form>
+    </section>
+  );
 };
 
 export default SignIn;

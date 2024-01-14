52cef5e05ae82b086d2d57ecb91cf7216b886db2
unknown <mwangiarthur777@gmail.com>
2024-01-14 17:44:52 +0300

    used react route dom and protected the event page route

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,39 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignIn from "./pages/signin/SignIn";
-// import Footer from "./components/footer/Footer";
-// import Navbar from "./components/navbar/Navbar";
-// import EventPage from "./pages/eventpage/EventPage";
-// import SignUp from "./pages/signup/SignUp";
-// import Home from "./pages/home/Home";
+import Home from "./pages/home/Home";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import EventPage from "./pages/eventpage/EventPage";
+import ProtectedRoute from "./ProtectedRoute";
 
 function App() {
+  const user: boolean = false;
   return (
     <>
-      <SignIn />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />}
+          />
+
+          <Route
+            path="/signin"
+            element={<SignIn />}
+          />
+          <Route
+            path="/event/:id"
+            element={
+              <ProtectedRoute user={user}>
+                <EventPage />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/ProtectedRoute.tsx
@@ -0,0 +1,16 @@
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ user, children }) => {
+  if (!user) {
+    return (
+      <Navigate
+        to="/signin"
+        replace
+      />
+    );
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;

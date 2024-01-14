497d2d3d7829fb116aecf05b469ba9fdf034afe8
unknown <mwangiarthur777@gmail.com>
2024-01-14 12:38:17 +0300

    Home page first section Design

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,10 +2,22 @@ import "./navbar.css";
 
 const Navbar = () => {
   return (
-    <div className="navbar flex items-center justify-center">
+    <div className="navbar bg-[#E5E6E9] flex items-center justify-center">
       <div className="container flex items-center justify-between">
         <div>
-          <p className="logo cursor-pointer">Smart Events</p>
+          <p className="text-xl text-[#492F6A] font-bold cursor-pointer">
+            Smart Events
+          </p>
+        </div>
+        <div className="flex items-center">
+          <div>
+            <i className="fa-solid fa-moon text-2xl"></i>
+          </div>
+          <div className="ml-4">
+            <button className="bg-white text-[#492F6A] px-6 py-2 rounded-md">
+              Sign in
+            </button>
+          </div>
         </div>
       </div>
     </div>
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,23 @@
+import "./home.css";
+
+const Home = () => {
+  return (
+    <div className="w-full flex items-center justify-center py-6">
+      <div className="container">
+        <h1 className="font-bold text-transparent text-3xl bg-clip-text bg-gradient-to-r from-indigo-900 to-blue-600">
+          Smart Events Hub.
+        </h1>
+        <h1 className="font-bold text-3xl">
+          Transforming Gatherings with Intelligence
+        </h1>
+        <p className="text-[#333333]">
+          Elevate your events to new heights with cutting-edge technology and
+          seamless intelligence, where every gathering becomes a transformative
+          experience at the Smart Events Hub.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;

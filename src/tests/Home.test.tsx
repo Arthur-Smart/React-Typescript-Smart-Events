9b76a5c93e4f7ea9c1276f9600545dcdaee2edd0
unknown <mwangiarthur777@gmail.com>
2024-01-16 11:59:48 +0300

    fixed import issue

--- src/tests/Home.test.tsx
+++ src/tests/Home.test.tsx
@@ -1,4 +1,4 @@
-import { describe, expect, it } from "vitest";
+import { describe, it } from "vitest";
 import Home from "../pages/home/Home";
 import { render, screen } from "@testing-library/react";
 // import SignIn from "../pages/signin/SignIn";

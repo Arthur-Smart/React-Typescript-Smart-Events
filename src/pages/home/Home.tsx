b22e1c93a913d5ea493ecd7eee6964b17a228538
unknown <mwangiarthur777@gmail.com>
2024-01-16 11:42:43 +0300

    added some framer motion animation

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,12 +2,28 @@ import { useContext } from "react";
 import Events from "../../components/events/Events";
 import "./home.css";
 import { ThemeContext } from "../../context/ThemeContext";
+import { motion } from "framer-motion";
 
 const Home = () => {
   const { state } = useContext(ThemeContext);
+
+  // Framer motion initialization
+  const bounceVariant = {
+    initial: { y: -100, opacity: 0 },
+    animate: {
+      y: 0,
+      opacity: 1,
+      transition: { type: "spring", duration: 2, bounce: 0.5 },
+    },
+  };
   return (
     <div className="w-full flex flex-col items-center justify-center py-6 px-4">
-      <section className="container py-4">
+      <motion.section
+        variants={bounceVariant}
+        initial="initial"
+        animate="animate"
+        className="container py-4"
+      >
         <h1 className="font-bold text-transparent text-3xl bg-clip-text bg-gradient-to-r from-indigo-900 to-blue-600">
           Smart Events Hub.
         </h1>
@@ -31,7 +47,7 @@ const Home = () => {
           seamless intelligence, where every gathering becomes a transformative
           experience at the Smart Events Hub.
         </p>
-      </section>
+      </motion.section>
 
       {/* Events section */}
       <section className="container py-4">
